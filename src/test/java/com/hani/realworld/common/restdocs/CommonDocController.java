93ccc972add5328b0ab1eeb267cbe8e24a16baa7
SeungHan <jk05018@naver.com>
2023-03-11 12:58:44 +0900

    feat(user): Rest Spring Docs 에러 Response 및 asciidoc 작성

--- src/test/java/com/hani/realworld/common/restdocs/CommonDocController.java
+++ src/test/java/com/hani/realworld/common/restdocs/CommonDocController.java
@@ -32,6 +32,22 @@ public class CommonDocController {
 			this.name = name;
 			this.email = email;
 		}
+
+		public String getName() {
+			return name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+
+		public String getEmail() {
+			return email;
+		}
+
+		public void setEmail(String email) {
+			this.email = email;
+		}
 	}
 
 }
