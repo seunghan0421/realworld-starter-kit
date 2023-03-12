eec66c22d78b6fee659aff78106d5c05c504e13b
SeungHan <jk05018@naver.com>
2023-03-12 16:55:16 +0900

    feat(user): followProfile 로직 및 테스트 완료

--- src/main/java/com/hani/realworld/user/application/service/FollowProfileService.java
+++ src/main/java/com/hani/realworld/user/application/service/FollowProfileService.java
@@ -1,10 +1,15 @@
 package com.hani.realworld.user.application.service;
 
+import static com.hani.realworld.user.domain.User.*;
+
 import javax.transaction.Transactional;
 
 import com.hani.realworld.common.annotation.UseCase;
 import com.hani.realworld.user.application.port.in.FollowProfileUseCase;
 import com.hani.realworld.user.application.port.in.result.ProfileResult;
+import com.hani.realworld.user.application.port.out.LoadProfileWithUserId;
+import com.hani.realworld.user.application.port.out.LoadProfileWithUsername;
+import com.hani.realworld.user.domain.Profile;
 
 import lombok.RequiredArgsConstructor;
 
@@ -13,8 +18,16 @@ import lombok.RequiredArgsConstructor;
 @UseCase
 public class FollowProfileService implements FollowProfileUseCase {
 
+	private final LoadProfileWithUsername loadProfileWithUsername;
+	private final LoadProfileWithUserId loadProfileWithUserId;
+
 	@Override
 	public ProfileResult followProfile(String username, Long userId) {
-		return null;
+		Profile target = loadProfileWithUsername.loadProfileWithUsername(username);
+		Profile base = loadProfileWithUserId.loadProfileWithUserId(new UserId(userId));
+
+		base.follow(target.getUser());
+
+		return ProfileResult.of(target, base.isFollowing(target.getUser()));
 	}
 }
--- src/test/java/com/hani/realworld/user/application/service/FollowProfileServiceTest.java
+++ src/test/java/com/hani/realworld/user/application/service/FollowProfileServiceTest.java
@@ -7,8 +7,6 @@ import static org.assertj.core.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.BDDMockito.*;
 
-import java.util.Optional;
-
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
@@ -17,7 +15,7 @@ import com.hani.realworld.user.application.port.out.LoadProfileWithUserId;
 import com.hani.realworld.user.application.port.out.LoadProfileWithUsername;
 import com.hani.realworld.user.domain.Profile;
 
-class GetProfileServiceTest {
+class FollowProfileServiceTest {
 
 	private final LoadProfileWithUsername loadProfileWithUsername =
 		Mockito.mock(LoadProfileWithUsername.class);
@@ -25,27 +23,28 @@ class GetProfileServiceTest {
 	private final LoadProfileWithUserId loadProfileWithUserId =
 		Mockito.mock(LoadProfileWithUserId.class);
 
-	private final GetProfileService getProfileService =
-		new GetProfileService(loadProfileWithUsername, loadProfileWithUserId);
+	private final FollowProfileService followProfileService =
+		new FollowProfileService(loadProfileWithUsername, loadProfileWithUserId);
 
-	// USER1이 USER2를 조회하는 상황
-	// 로그인 O, follow X
+	// USER1가 USER2 follow - 성공
 	@Test
-	void getProfile_withLogin_Succeeds() {
+	void followProfile_Succeeds() {
 		// given
 		Profile target = givenAnProfileOfUser2();
 		Profile base = givenAnProfileOfUser1();
 
 		String targetUsername = target.getUser().getUsername();
-		Optional<Long> baseUserId = Optional.of(base.getUser().getId().getValue());
+		Long baseUserId = base.getUser().getId().getValue();
 
 		given(loadProfileWithUsername.loadProfileWithUsername(eq(targetUsername)))
 			.willReturn(target);
-		given(loadProfileWithUserId.loadProfileWithUserId(eq(new UserId(baseUserId.get()))))
+		given(loadProfileWithUserId.loadProfileWithUserId(eq(new UserId(baseUserId))))
 			.willReturn(base);
+		given(base.isFollowing(target.getUser()))
+			.willReturn(true);
 
 		// when
-		ProfileResult result = getProfileService.getProfile(targetUsername, baseUserId);
+		ProfileResult result = followProfileService.followProfile(targetUsername, baseUserId);
 
 		// then
 		assertThat(result.getUsername()).isEqualTo(USER2.getUsername());
@@ -55,33 +54,35 @@ class GetProfileServiceTest {
 
 		then(loadProfileWithUserId).should().loadProfileWithUserId(eq(USER1.getId()));
 		then(loadProfileWithUsername).should().loadProfileWithUsername(eq(USER2.getUsername()));
-		then(base).should().isFollowing(USER2);
+		then(base).should().follow(eq(USER2));
+		then(base).should().isFollowing(eq(USER2));
 	}
 
-	// USER1이 USER2를 조회하는 상황
-	// 로그인 X, follow X
+	// USER1가 USER2 follow - 실패 : 이미 팔로우 되어 있음
 	@Test
-	void getProfile_withOutLogin_Succeeds() {
+	void followProfile_Failure() {
 		// given
 		Profile target = givenAnProfileOfUser2();
+		Profile base = givenAnProfileOfUser1();
 
 		String targetUsername = target.getUser().getUsername();
-		Optional<Long> baseUserId = Optional.empty();
+		Long baseUserId = base.getUser().getId().getValue();
 
 		given(loadProfileWithUsername.loadProfileWithUsername(eq(targetUsername)))
 			.willReturn(target);
+		given(loadProfileWithUserId.loadProfileWithUserId(eq(new UserId(baseUserId))))
+			.willReturn(base);
+		// TODO: 여기 왜 eq(target.getUser()) 안되는지 확인해야함
+		doThrow(new IllegalStateException()).when(base).follow(eq(USER2));
 
 		// when
-		ProfileResult result = getProfileService.getProfile(targetUsername, baseUserId);
+		assertThatThrownBy(() -> followProfileService.followProfile(targetUsername, baseUserId))
+			.isInstanceOf(IllegalStateException.class);
 
 		// then
-		assertThat(result.getUsername()).isEqualTo(USER2.getUsername());
-		assertThat(result.getBio()).isEqualTo(USER2.getBio());
-		assertThat(result.getImage()).isEqualTo(USER2.getImage());
-		assertThat(result.isFollowing()).isFalse();
-
+		then(loadProfileWithUserId).should().loadProfileWithUserId(eq(USER1.getId()));
 		then(loadProfileWithUsername).should().loadProfileWithUsername(eq(USER2.getUsername()));
+		then(base).should().follow(eq(USER2));
 	}
-
 
 }
